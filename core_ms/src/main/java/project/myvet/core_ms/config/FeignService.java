117bf15868a64281ff333c31a518fb78984f67bd
BigTuck <nathan.tucker92@gmail.com>
2024-10-10 19:14:41 -0500

    Created FeignService.java

--- core_ms/src/main/java/project/myvet/core_ms/config/FeignService.java
+++ core_ms/src/main/java/project/myvet/core_ms/config/FeignService.java
@@ -1,10 +1,14 @@
 package project.myvet.core_ms.config;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "billing_ms")
 public interface FeignService {
 
-	
+	@GetMapping("/bill")
+	ResponseEntity<Double> calculateBill(@RequestParam Double visitRate, @RequestParam Integer numberPets);
 	
 }
